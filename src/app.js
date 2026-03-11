af2f5c389eb6f28c44ca8c53a129016e09f4b74d
Chiri Nina <echiri@cca.edu.bo>
2026-03-11 15:10:10 -0400

    Inicio

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
 import express from 'express';
 import morgan from 'morgan';
-import usersRoutes from './routes/users.route.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import usersRoutes from './routes/users.route.js';
 import taskRoutes from './routes/task.route.js';
 import authRoute from './routes/auth.route.js';
+
 const app = express();
+
+// Configuración de __dirname para ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middlewares
 app.use(morgan('combined'));
 app.use(express.json());
 
-// Routes
-app.use('/api/users/', usersRoutes)
-app.use('/api/login/', authRoute)
-app.use('/api/tasks/', taskRoutes)
+// Rutas de la API
+app.use('/api/users', usersRoutes);
+app.use('/api/login', authRoute);
+app.use('/api/tasks', taskRoutes);
 
+// Servir index.html desde la raíz del proyecto
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html')); // <<-- subir un nivel desde src
+});
 
 export default app;
